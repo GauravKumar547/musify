230f0cc0af78457d60c05f5253868bb96b5c58e6
Gaurav Kumar <Gkumar@s7works.io>
2023-10-23 20:44:18 +0530

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Musify",
+    description: "Listen to music",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={font.className}>{children}</body>
+        </html>
+    );
+}
