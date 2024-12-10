84551ce3929eb502f62213062e75049d0c31690f
Gaurav Kumar <59726822+GauravKumar547@users.noreply.github.com>
2024-12-10 18:14:17 +0530

    Update next.config.js for new project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["odlvxezqrthsyclfusxo.supabase.co"],
+        domains: ["meqrlgmvadrrxxaynrnj.supabase.co"],
     },
 };
 
